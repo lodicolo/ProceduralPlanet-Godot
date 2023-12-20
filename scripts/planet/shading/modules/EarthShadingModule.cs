a1b6f04ba4384338dc088eb8903bf50fce6a0b03
Robbie Lodico <pandacoder@pm.me>
2023-12-19 22:45:18 -0500

    free crash fix

--- scripts/planet/shading/modules/EarthShadingModule.cs
+++ scripts/planet/shading/modules/EarthShadingModule.cs
@@ -26,8 +26,11 @@ public partial class EarthShadingModule : ShadingDataModule
         base.Dispose(disposing);
 
         (var renderingDevice, _renderingDevice) = (_renderingDevice, default);
-        renderingDevice?.Free();
-        renderingDevice?.Dispose();
+        if (renderingDevice is { NativeInstance: not default(nint) })
+        {
+            renderingDevice.Free();
+            renderingDevice.Dispose();
+        }
     }
 
     public override UVPairs Run(RandomNumberGenerator rng, Vector3[] vertices)
